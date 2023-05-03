7f85c7a430b4ed61d9c4c3cafdda20a0f1cbf6f4
vivanshu-vvvg <vivanshugupta2003.m@gmail.com>
2023-05-03 22:56:02 +0530

    Q3_A_B
    Adding Question no.3 Both parts.

--- src/Q3_A.js
+++ src/Q3_A.js
@@ -1,1 +1,25 @@
-//03 A}Write a JavaScript program using function to find out whether a number is a palindrome or not. The console must display The number is a palindrome if it is a palindrome otherwise it must display 'The number is not a palindrome' 
+//03_A}Write a JavaScript program using function to find out whether a number is a palindrome or not. The console must display The number is a palindrome if it is a palindrome otherwise it must display 'The number is not a palindrome' 
+
+function isPalindrome(number) {
+    const reverseNumber = parseInt(number.toString().split('').reverse().join(''));
+    if (number === reverseNumber) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+  const number = 1.1; // Example number to check for palindrome
+  if (isPalindrome(number)) {
+    console.log('The number is a palindrome');
+  } else {
+    console.log('The number is not a palindrome');
+  }
+  
+  /*
+  A palindrome is a word, phrase, number, or other sequence of characters that reads the same forward and backward. For example, "racecar", "level", and "deified" are all palindromic words.
+
+In the case of numbers, a palindrome number is a number that remains the same when its digits are reversed. For example, 121, 1221, and 12321 are all palindromic numbers.
+
+Palindromes can occur in various contexts, such as words, phrases, sentences, numbers, DNA sequences, and more. They have been studied in mathematics, linguistics, computer science, and other fields.
+*/
--- src/Q3_B.js
+++ src/Q3_B.js
@@ -1,1 +1,39 @@
 //Q3_B) Demonstrate the concept of middleware.
+
+/*
+
+In the context of web development, middleware is a software component that sits between a web application and the server or client, processing incoming and outgoing HTTP requests and responses. Middleware can add, modify or remove data from requests and responses, as well as perform other tasks such as authentication, logging, caching, error handling, and more. In this way, middleware provides a flexible and modular way to enhance the functionality and performance of web applications.
+
+Here's an example of how middleware works in a Node.js web application:
+*/
+const express = require('express');
+const app = express();
+
+// Middleware function 1
+app.use((req, res, next) => {
+  console.log('Middleware 1: Request received');
+  next();
+});
+
+// Middleware function 2
+app.use((req, res, next) => {
+  console.log('Middleware 2: Request processed');
+  next();
+});
+
+// Route handler
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Start the server
+app.listen(3010, () => {
+  console.log('Server started on port 3010');
+});
+
+
+/*
+In this example, the app.use() method is used to register two middleware functions before the route handler. These middleware functions have access to the req and res objects, as well as a next function that allows them to pass control to the next middleware function or the route handler.
+
+When a request is made to the server, the first middleware function logs a message to the console, then calls the next() function to pass control to the next middleware function. The second middleware function logs another message and calls next() again, which then passes control to the route handler. Finally, the route handler sends a response back to the client.
+*/
